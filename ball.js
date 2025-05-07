992acb7af6b56d86663249a925eaae84a2f0c240
dani783i <danielknudsendinfar@gmail.com>
2025-05-07 11:24:17 +0200

    fjerner noget fra kommentare der ikke skal være der.
    der bliver fjernet tal i kommentar da vi kun brugte det til rækkefølge.

--- ball.js
+++ ball.js
@@ -46,7 +46,7 @@ class Ball{
             const minDist = this.radius + other.radius;
         
             if (distance < minDist) {
-                // 1. Separér kuglerne så de ikke overlapper
+                //Separér kuglerne så de ikke overlapper
                 let overlap = 0.5 * (minDist - distance + 0.1); // Lidt ekstra for at sikre separation
                 let nx = dx / distance;
                 let ny = dy / distance;
@@ -56,14 +56,14 @@ class Ball{
                 other.x += nx * overlap;
                 other.y += ny * overlap;
         
-                // 2. Beregn de oprindelige hastigheder og retninger
+                //Beregn de oprindelige hastigheder og retninger
                 let angle = Math.atan2(dy, dx);
                 let speed1 = Math.sqrt(this.velocity.x ** 2 + this.velocity.y ** 2);
                 let speed2 = Math.sqrt(other.velocity.x ** 2 + other.velocity.y ** 2);
                 let direction1 = Math.atan2(this.velocity.y, this.velocity.x);
                 let direction2 = Math.atan2(other.velocity.y, other.velocity.x);
         
-                // 3. Roter hastigheder til kollisionsakse
+                //Roter hastigheder til kollisionsakse
                 let velocity1 = {
                     x: speed1 * Math.cos(direction1 - angle),
                     y: speed1 * Math.sin(direction1 - angle)
@@ -73,19 +73,19 @@ class Ball{
                     y: speed2 * Math.sin(direction2 - angle)
                 };
         
-                // 4. Brug elastisk kollision i 1D (langs kollisionslinjen)
+                //Brug elastisk kollision i 1D (langs kollisionslinjen)
                 let massSum = this.mass + other.mass;
                 let newVelX1 = ((this.mass - other.mass) * velocity1.x + 2 * other.mass * velocity2.x) / massSum;
                 let newVelX2 = ((other.mass - this.mass) * velocity2.x + 2 * this.mass * velocity1.x) / massSum;
         
-                // 5. Tilføj lidt energitab som friktion (mellem 0.9 og 1)
+                //Tilføj lidt energitab som friktion (mellem 0.9 og 1)
                 const damping = 0.95;
                 newVelX1 *= damping;
                 newVelX2 *= damping;
                 velocity1.y *= damping;
                 velocity2.y *= damping;
         
-                // 6. Roter tilbage til det oprindelige koordinatsystem
+                //Roter tilbage til det oprindelige koordinatsystem
                 this.velocity.x = newVelX1 * Math.cos(angle) + velocity1.y * Math.cos(angle + Math.PI / 2);
                 this.velocity.y = newVelX1 * Math.sin(angle) + velocity1.y * Math.sin(angle + Math.PI / 2);
                 other.velocity.x = newVelX2 * Math.cos(angle) + velocity2.y * Math.cos(angle + Math.PI / 2);
